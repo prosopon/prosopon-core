8ff2dd2e1c3a0e57abd6c29380e530b6e8576123
Matt Bierner <mattbierner@gmail.com>
2012-03-23 13:48:28 -0400

    Updated config file with visibility attributes.

--- include/prosopon_config.h
+++ include/prosopon_config.h
@@ -2,9 +2,26 @@
 #define prosopon_config_h
 
 
-#define PRO_API	extern
-#define PRO_LIBCORE extern
-#define PRO_INTERNAL extern
+#if __llvm__ || __GNUC__ >= 4
+    /**
+     * Marks a symbol as part of the public prosopon API. 
+     */
+    #define PRO_API	extern __attribute__ ((visibility ("default")))
+
+    /**
+     * Marks a symbol as part of the public prosopon core library API. 
+     */
+    #define PRO_LIBCORE extern __attribute__ ((visibility ("default")))
+
+    /**
+     * Marks a symbol as internal, not intended for public use.
+     */
+    #define PRO_INTERNAL extern __attribute__ ((visibility ("hidden")))
+#else
+    #define PRO_API	extern
+    #define PRO_LIBCORE extern
+    #define PRO_INTERNAL extern
+#endif
 
 
 #endif
