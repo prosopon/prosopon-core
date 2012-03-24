1c911eb7564972276f7c9378d7dc8552b8104be2
Matt Bierner <mattbierner@gmail.com>
2012-03-24 13:17:45 -0400

    Added empty constructor functions.

---
+++ src/pro_constructor.c
@@ -0,0 +1,16 @@
+#include "pro_constructor.h"
+
+#include "prosopon.h"
+
+
+
+PRO_API pro_lookup* (pro_constructor_create) (pro_state* s, pro_constructor* c, void* data)
+{
+    return 0;
+}
+
+
+PRO_API pro_lookup* (pro_constructor_call) (pro_state* s, pro_lookup* constructor, pro_lookup_list* arguments)
+{
+    return 0;
+}
---
+++ src/pro_constructor.h
@@ -0,0 +1,6 @@
+#ifndef prosopon_pro_constructor_h
+#define prosopon_pro_constructor_h
+
+
+
+#endif
