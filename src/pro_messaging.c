cb358eeb56bcca8a076b5fa586a26e5e754ccc34
Matt Bierner <mattbierner@gmail.com>
2012-04-08 15:55:47 -0400

    Allow empty actor behavior, self bound to actor in behavior env.

--- src/pro_messaging.c
+++ src/pro_messaging.c
@@ -8,9 +8,18 @@ PRO_INTERNAL void pro_deliver_message(pro_state_ref s,
     pro_ref actor, pro_ref message)
 {
     pro_object* actor_obj = pro_dereference(s, actor);
-    pro_push_env(s, actor_obj->value.actor.env);
-    pro_behavior* behavior = actor_obj->value.actor.behavior;  
-    behavior(s, actor, message, actor_obj->value.actor.data);
-    pro_pop_env(s);
+    pro_behavior* behavior = actor_obj->value.actor.behavior;
+    if (behavior)
+    {
+        // setup environment
+        pro_push_env(s, actor_obj->value.actor.env);
+        pro_bind(s, actor, "self"); // setup self
+        
+        // call the behavior
+        behavior(s, actor, message, actor_obj->value.actor.data);
+        
+        // teardown the environment
+        pro_pop_env(s);
+    }
 }
 
