5816f8af962e2280d06c6750fab16b70a0eb383d
Matt Bierner <mattbierner@gmail.com>
2012-04-15 22:36:55 -0400

    Adding code documentation, reduced number of env created by constructors.

--- src/pro_constructor.c
+++ src/pro_constructor.c
@@ -3,6 +3,7 @@
 #include "pro_env.h"
 #include "pro_common.h"
 #include "pro_object.h"
+#include "pro_lookup.h"
 
 
 PRO_API pro_error pro_constructor_create(pro_state_ref s,
@@ -19,14 +20,12 @@ PRO_API pro_error pro_constructor_create(pro_state_ref s,
     *obj = pro_object_new(s, PRO_CONSTRUCTOR_TYPE, 1);
     (*obj)->value.constructor.constructor = c;
    
+    // Set the user data
     pro_retain(s, ud);
     (*obj)->value.constructor.data = ud;
     
-    // Create a new env for this actor with current_env as parent
-    pro_env_create(s, current_env, &((*obj)->value.constructor.env));
-    
-    // Release current env
-    pro_env_release(s, current_env);
+    // Save the environment
+    (*obj)->value.constructor.env = current_env;
     
     *constructor = lookup;
     return PRO_OK;
@@ -41,16 +40,23 @@ PRO_API pro_error pro_constructor_call(pro_state_ref s,
     
     pro_object* obj = pro_dereference(s, constructor);
     
-    pro_env_ref env;
-    pro_env_create(s, obj->value.constructor.env, &env);
-    pro_push_env(s, env);
-    pro_env_release(s, env);
+    // Create a new env for the call with the parent from the save constructor
+    {
+        pro_env_ref env;
+        pro_env_create(s, obj->value.constructor.env, &env);
+        pro_push_env(s, env);
+        pro_env_release(s, env);
+    }
     
+    // Call the constructor implementation
     pro_constructor* impl = obj->value.constructor.constructor;
     pro_ref ud = obj->value.constructor.data;
     pro_ref out = impl(s, arguments, ud);
-    PRO_API_ASSERT(PRO_EMPTY_REF != out, PRO_CONSTRUCTOR_ERROR);
     
+    // Error if constructor returns nothing
+    PRO_API_ASSERT(!pro_lookup_equal(s, out, PRO_EMPTY_REF), PRO_CONSTRUCTOR_ERROR);
+    
+    // Release the env
     pro_pop_env(s);
     
     *result = out;
